213335d5b7aaf126900fb0b38b6960c9808aaac6
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-12-03 14:25:03 +0700

    logical

--- js/logical.js
+++ js/logical.js
@@ -4,3 +4,14 @@ var c = a || b;
 var d = b && c;
 
 console.log(c, d); // Output: true false
+
+
+var result = 'A' && 'B' && 'C' && 'D' && 'E' && NaN
+console.log(result);
+
+
+if (result) {
+    console.log('error');
+} else {
+    console.log('success');
+}
