eecf9490d80215c0a2d9ad43ab9fdc9aa13bb0d5
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-12-04 03:00:29 +0700

    string

---
+++ js/string.js
@@ -0,0 +1,20 @@
+
+var fullName = 'Tran Thi Cam Hoa';
+const email = 'chtran03042004@gmail.com'
+
+var options = new String('Hello')
+
+console.log(typeof options)
+//thay vì là string thì nó ra là object
+
+var lorem = "lorem ipsum dolor sit amet consectetur con laoreet inter sapien"
+console.log(lorem.length)
+//63
+
+var firstName = 'Hoa';
+var lastName = 'Tran';
+
+console.log(`Toi la: ${firstName} ${lastName}`);
+// firstName và lastName nó tự hiểu là kiểu string 
+
+/*---------------------------cách làm việc với chuỗi-------------------------------------*/
