474bb63585855908bd9fd27461ba2e37df9e8023
Hoa0304 <tranthicamhoa2k4@gmail.com>
2024-01-12 21:28:15 +0700

    includes method

---
+++ js/includes.js
@@ -0,0 +1,11 @@
+var title = "Hoa nong tinh"
+
+console.log(title.includes("Hoa"))
+
+var cars = ['Rolls-Royce', 'Mercedes', 'Lexus', 'BMW', 'Audi'];
+
+function checkCar(cars) {
+   return cars.includes('Mercedes', 2);
+}
+
+console.log(checkCar(cars)); // Output: ?
